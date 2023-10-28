0238bdbafb0f7096c7fdb9152668941e35cde1bf
Артём Рассудов <mr.rassudov@mail.ru>
2023-10-28 11:53:05 +0700

    - Создан fragment_crime_list.xml - fragment_crime_list.xml добавлен в MainActivity.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-extensions:2.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.0.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
